14fc1aa63b2023759544bd6f2131655a25715ef5
greciacas <xgreciacastillox@gmail.com>
2021-11-02 23:59:57 -0400

    seed data, tested routeson postman

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
-  has_many :posts, :comments
+  has_many :posts
+  has_many :comments
 
   validates :username, presence: true, uniqueness: true
   validates :password, length: { minimum: 6 }
