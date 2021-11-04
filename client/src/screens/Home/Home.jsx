bccb830e510b71ba6ac3c54cb7cd72e2c896fe40
greciacas <xgreciacastillox@gmail.com>
2021-11-04 13:46:52 -0400

    updated lock file for deployment

--- client/src/screens/Home/Home.jsx
+++ client/src/screens/Home/Home.jsx
@@ -1,6 +1,15 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
 export default function Home() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(()=> {
+    axios.get('/')
+  }, [])
+  
   return (
-    <div className='post'>
+    <div className='posts'>
       <h3>post components here</h3>
     </div>
   )
